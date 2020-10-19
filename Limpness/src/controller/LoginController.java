84c4ab8b0011cef836d08d3242c498189f225779
PauloHenriqueG <paulogoncalves201926@outlook.com>
2020-10-19 20:41:25 -0300

    Login

--- Limpness/src/controller/LoginController.java
+++ Limpness/src/controller/LoginController.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -28,15 +29,44 @@ public class LoginController extends Application {
 	    private Button btnsair;
 
 	    @FXML
-	    void Login(ActionEvent event) {
-
+	    public void Login(ActionEvent event) {
+          if (txfUsuario.getText().equals("Limpness") && txfSenha.getText().equals("1234")) {
+        	  Stage stage = new Stage();
+        	  
+        	  try {
+        			AnchorPane pane = (AnchorPane) FXMLLoader.load(getClass().getResource("/ViewLogin/PRINCILPAL.fxml"));
+        			Scene sc = new Scene(pane);
+        			stage.setScene(sc);
+        			stage.show();
+        		} catch (IOException e) {
+        			e.printStackTrace();
+        		}
+        	 
+        	  Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        	  alert.setTitle("teste blabla");
+        	  alert.setHeaderText("Bem vindo ao Limpness");
+        	  alert.show();
+        	  
+        	  
+          }else {
+        	  Alert alert = new Alert(Alert.AlertType.ERROR);
+        	  alert.setTitle("teste blabla");
+        	  alert.setHeaderText("Senha ou Email esta incorreto.");
+        	  alert.show();
+          }
 	    }
 
 	    @FXML
 	    void sair(ActionEvent event) {
-
+              System.exit(0);
 	    }
 	
+	    
+	    
+	    
+	    
+	    
+	    
 	public void execute() {
 		launch();
 	}
